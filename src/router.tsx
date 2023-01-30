386645adfe7327659210f5fbe067f8f89ee560b1
chestnut90 <cowzon90@gmail.com>
2023-01-30 17:05:40 +0900

    add NotFound page component

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
 import Root from './components/Root';
+import NotFound from './routes/NotFound';
 
 const router = createBrowserRouter([{
     path: '/',
     element: <Root />,
+    errorElement: <NotFound />,
     children: [
         {
             path: '/Home', element: <Home />
