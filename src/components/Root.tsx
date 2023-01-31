156635637a3ed0aaeeea9f8f2a5e1f8392ac0010
chestnut90 <cowzon90@gmail.com>
2023-01-31 13:16:30 +0900

    add Header on Root with react-icon

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,12 +1,27 @@
+import { Box, Button, HStack, VStack } from "@chakra-ui/react";
+import { FaAirbnb } from "react-icons/fa";
 import { Outlet } from "react-router-dom";
 
+// HStack spacing => space between items, rem
+// find Default Theme for color
 export default function Root() {
     return (
-        <div>
-            <h1>Header Section</h1>
+        <Box >
+            <HStack paddingY={"5"} paddingX={"5"}
+                borderBottomWidth={1}
+                justifyContent={"space-between"}>
+                <Box color={"red.500"}>
+                    <FaAirbnb size={"2rem"} />
+
+                </Box>
+                <HStack spacing={"2rem"}>
+                    <Button>Sign in</Button>
+                    <Button colorScheme={"red"}>Sign up</Button>
+                </HStack>
+            </HStack>
             <Outlet />
             <h1>Footer Section</h1>
-        </div>
+        </Box >
     )
 }
 
