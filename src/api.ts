895852a91877f9636221e9f8db241b8be3b5c3cd
chestnut90 <cowzon90@gmail.com>
2023-02-16 12:23:17 +0900

    error fix, change deployment backend url

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ import { formatDate } from "./lib/utils";
 
 const baseURL = process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000/api/v1/"
-    : "https://backend.https://chestnut-clone.click/api/v1/";
+    : "https://backend.chestnut-clone.click/api/v1/";
 
 const instance = axios.create({
     baseURL: baseURL,
